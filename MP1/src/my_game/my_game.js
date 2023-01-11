51e7bef50f6c8b43667a1ddfd878e21d473431ff
Tonneh <88176459+Tonneh@users.noreply.github.com>
2023-01-11 12:07:40 -0800

    added more to the house scene

--- MP1/src/my_game/my_game.js
+++ MP1/src/my_game/my_game.js
@@ -15,37 +15,54 @@ class MyGame {
         // Step B: Clear the canvas
         engine.clearCanvas(pink);
 
-        // Step C: Draw the square in red
+        this.drawSquareRow();
+        this.drawTriangleRow();
+        this.drawCircleRow();
+        //this.drawHouse();
+    }
 
+    drawSquareRow() {
         /* Drawing the top row */
         engine.drawSquare(black, [col1, row1], scale1);
         engine.drawSquare(brown, [col2, row1], scale2);
         engine.drawSquare(red, [col3, row1], scale3);
         engine.drawSquare(green, [col4, row1], scale4);
         engine.drawSquare(blue, [col5, row1], scale5);
+    }
 
+    drawTriangleRow() {
         /* Drawing Middle Row */
         engine.drawTriangle(black, [col1, row2], scale1);
         engine.drawTriangle(brown, [col2, row2], scale2);
         engine.drawTriangle(red, [col3, row2], scale3);
         engine.drawTriangle(green, [col4, row2], scale4);
         engine.drawTriangle(blue, [col5, row2], scale5);
+    }
 
+    drawCircleRow() {
         /* Drawing Last Row */
         engine.drawCircle(black, [col1, row3], scale1);
         engine.drawCircle(brown, [col2, row3], scale2);
         engine.drawCircle(red, [col3, row3], scale3);
         engine.drawCircle(green, [col4, row3], scale4);
         engine.drawCircle(blue, [col5, row3], scale5);
+    }
 
-        /* Code for Drawing a House
+    drawHouse() {
+        /* Code for Drawing a House */
+
+        /* Base House */
         engine.drawSquare(brown, [0, -0.5], [1, 1]);
         engine.drawTriangle(brown, [0, 0.5], [1, 1]);
-        engine.drawSquare(green, [0, -0.7], [0.25,0.6]);
+
+        /* Door */
+        engine.drawSquare(green, [0, -0.7], [0.25, 0.6]);
         engine.drawCircle(black, [0.07, -0.75], [0.05, 0.05]);
         engine.drawSquare(black, [0.125, -0.7], [0.01, 0.6]);
         engine.drawSquare(black, [-0.125, -0.7], [0.01, 0.6]);
         engine.drawSquare(black, [0, -0.4], [0.25, 0.01]);
+
+        /* Window */
         engine.drawSquare(white, [0, 0.3], [0.3, 0.3]);
         engine.drawSquare(black, [0, 0], [1, 0.01]);
         engine.drawSquare(black, [0, 0.45], [0.3, 0.01]);
@@ -53,18 +70,32 @@ class MyGame {
         engine.drawSquare(black, [0, 0.3], [0.01, 0.3]);
         engine.drawSquare(black, [0.15, 0.3], [0.01, 0.3]);
         engine.drawSquare(black, [-0.15, 0.3], [0.01, 0.3]);
-         */
+
+        /* Draw the Sun */
+        engine.drawCircle(yellow, [-1.0, 0.9], [0.5, 0.5]);
+
+        /* Cloud */
+        engine.drawCircle(white, [0.7, 0.7], [0.2, 0.2]);
+        engine.drawCircle(white, [0.6, 0.7], [0.2, 0.2]);
+        engine.drawCircle(white, [0.8, 0.7], [0.2, 0.2]);
+        engine.drawCircle(white, [0.8, 0.82], [0.2, 0.2]);
+        engine.drawCircle(white, [0.7, 0.82], [0.2, 0.2]);
+        engine.drawCircle(white, [0.6, 0.82], [0.2, 0.2]);
+
+
+
     }
 }
 
 /* Colors */
 let black = [0, 0, 0, 1];
-let red = [1, 0 , 0, 1];
+let red = [1, 0, 0, 1];
 let brown = [0.70, 0.40, 0.11, 1];
 let pink = [1, 0.75, 0.80, 1];
 let blue = [0, 0, 1, 1];
 let green = [0, 1, 0, 1];
 let white = [1, 1, 1, 1];
+let yellow = [1, 1, 0, 1];
 
 /* Positions */
 let row1 = 0.6; // Top Row;
@@ -84,6 +115,6 @@ let scale3 = [0.2, 0.25];
 let scale4 = [0.35, 0.35];
 let scale5 = [0.55, 0.55];
 
-window.onload = function() {
+window.onload = function () {
     new MyGame('GLCanvas');
 }
